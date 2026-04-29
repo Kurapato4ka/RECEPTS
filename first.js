575cc930880b4afdc71410f7e18a59fd8d7d2c5f
Kurapato4ka <kirill.kurbatov.phone@gmail.com>
2026-04-29 15:29:30 +0300

    prelesson

---
+++ first.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const triggerImage = document.getElementById('triggerImage');
+    const dropdownContent = document.getElementById('myDropdown');
+
+    triggerImage.addEventListener('click', function(event) {
+        event.stopPropagation();
+        dropdownContent.classList.toggle('show');
+    });
+
+    window.addEventListener('click', function(event) {
+        if (dropdownContent.classList.contains('show')) {
+            if (event.target !== triggerImage && !dropdownContent.contains(event.target)) {
+                dropdownContent.classList.remove('show');
+            }
+        }
+    });
+
+});
